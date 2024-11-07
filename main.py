9861e00cc1bf5968d1656eead10df4916bdbac09
CamdenSmith04 <ctsmith0319@gmail.com>
2024-11-07 09:07:01 -0500

    Set-Up

--- main.py
+++ main.py
@@ -6,25 +6,22 @@ file = open(".password.txt", "r")
 username = file.readline()[:-1]
 password = file.readline()[:-1]
 r.login(username, password)
+print("Sign in successful.")
 
 # Industry Specific Stocks
 steel = ["STLD", "NUE", "CLF", "X"]
+materials = []
 
+# Collection of ETFs
+etfs_description = {"XLB" : "Materials Select Sector SPDR Fund", "SLX" : "VanEck Steel ETF"}
+etfs = ["XLB", "SLX"]
 
-
-# Collective Stocks
+# Collection of Stocks
 stocks = []
-
 for i in range(len(steel)):
     stocks.append(steel[i])
 
-print(stocks)
+# Function to get the P/E ratio of a stock
+def get_PE(stock):
+    return r.get_fundamentals(stock, info="pe_ratio")
 
-
-
-
-
-
-
-
-print("Done")
